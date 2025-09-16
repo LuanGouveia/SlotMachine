b55d77647807d3528ea0c660f78ab02666fd3076
damn, it’s Gouveia. <99460625+LuanGouveia@users.noreply.github.com>
2025-09-16 12:28:39 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,17 +1,25 @@
 const chance = new Chance()
 
+const spinSound = new Audio("sounds/spinning.wav")
+const winSound = new Audio("sounds/win.wav")
+
 let win = 0
 let lost = 0
 let tries = 0
 
+let flamengo = 0
+let fluminense = 0
+let vasco = 0
+
 const btnPlay = document.getElementById("btnPlay")
-if(btnPlay){btnPlay.addEventListener("click", play)}
+if(btnPlay){btnPlay.addEventListener("click", playGame)}
 
 let barChart; 
 let pieChart; 
 
-function play(){
+function playGame(){
     btnPlay.disabled = true;
+    spinSound.play()
     
     firstCard = document.getElementById("idFirstImage")
     secondCard = document.getElementById("idSecondImage")
@@ -19,9 +27,9 @@ function play(){
 
     const cards = [firstCard, secondCard, thirdCard]
 
-    const firstImage = "images/cachorro.jpg"
-    const secondImage = "images/elefante.jpg"
-    const thirdImage = "images/galo.jpg"
+    const firstImage = "images/flamengo-logo-2020.svg"
+    const secondImage = "images/fluminense-logo-escudo.svg"
+    const thirdImage = "images/vasco-da-gama-logo-2021.svg"
 
     const images = [firstImage, secondImage, thirdImage]
 
@@ -35,7 +43,7 @@ function play(){
         return setInterval(() => {
             const randomImage = chance.pickone(images);
             card.src = randomImage;
-        }, 50);
+        }, 20);
     });
 
     function stopCard(cardIndex,cardInterval){
@@ -52,32 +60,60 @@ function play(){
         btnPlay.disabled = false;
 
         if(cards[0].src == cards[1].src && cards[1].src == cards[2].src){
-        document.getElementById("result").innerHTML = "Parabéns, você ganhou!" 
-        win++
+            winSound.play()
+            setTimeout(() => {
+                winSound.pause();
+                winSound.currentTime = 0;
+            }, 1500);
+            document.getElementById("result").innerHTML = "Parabéns, você ganhou!" 
+            win++
         }else{
             document.getElementById("result").innerHTML = "Poxa, você perdeu, tente novamente!" 
             lost++
         }
         tries++
+
+        for(let element of cards){
+            if(element.src.includes(firstImage)){
+                flamengo++
+            }else if(element.src.includes(secondImage)){
+                fluminense++
+            }else{
+                vasco++
+            }
+        }
+
         localStorage.setItem("tries", tries);
         localStorage.setItem("win", win);
         localStorage.setItem("lost", lost);
+
+        localStorage.setItem("flamengo",flamengo)
+        localStorage.setItem("fluminense",fluminense)
+        localStorage.setItem("vasco",vasco)
 
         if (barChart && pieChart) {
         barChart.data.datasets[0].data = [tries, lost, win];
         barChart.update();
 
-        pieChart.data.datasets[0].data = [lost, win];
+        pieChart.data.datasets[0].data = [flamengo, fluminense, vasco];
         pieChart.update();
         }
+
+        spinSound.pause()
+        spinSound.currentTime = 0
     }, 3000);
 }
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    let tries = parseInt(localStorage.getItem("tries")) || 0;
-    let win = parseInt(localStorage.getItem("win")) || 0;
-    let lost = parseInt(localStorage.getItem("lost")) || 0;
+    flamengo = parseInt(localStorage.getItem("flamengo")) || 0;
+    fluminense = parseInt(localStorage.getItem("fluminense")) || 0;
+    vasco = parseInt(localStorage.getItem("vasco")) || 0;
+
+
+    tries = parseInt(localStorage.getItem("tries")) || 0;
+    win = parseInt(localStorage.getItem("win")) || 0;
+    lost = parseInt(localStorage.getItem("lost")) || 0;
 
     const ctxBar = document.getElementById('graphicBar').getContext('2d')
     const ctxPie = document.getElementById('graphicPie').getContext('2d')
@@ -128,13 +164,14 @@ document.addEventListener("DOMContentLoaded", () => {
     type: 'pie',   
 
     data: {
-    labels: ['Perdedores', 'Vencedores'],
+    labels: ['Flamengo', 'Fluminense', "Vasco"],
 
     datasets: [{              
-        data: [lost, win],
+        data: [flamengo, fluminense, vasco],
         backgroundColor: [      
             "rgba(197, 13, 13, 0.83)",
-            "rgba(16, 131, 31, 0.84)"
+            "rgba(16, 131, 31, 0.84)",
+            "rgba(255, 255, 255, 0.83)"
         ],
         borderWidth: 1
         }]
